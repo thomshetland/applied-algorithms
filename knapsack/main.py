d5912da3b2256f21294975a04077e6e10c1b262c
Thomas Hetland <thomas.hetland@outlook.com>
2026-03-01 11:44:43 +0100

    Finished Knapsack

--- knapsack/main.py
+++ knapsack/main.py
@@ -12,19 +12,11 @@ def create_objects(number_of_objects):
         objects[key] = [weight, value]
     return objects
 
-def sort_objects(object, choice):
-    sorted_objects = []
-    for i in range(len(object)):
-        if choice ==  "Weight":
-            max_key = max(object, key=lambda k: object[k][0])
-            print(f"MAXKEY: {max_key}")
-            sorted_objects.append(max_key)
-            object.pop(max_key)
-        else:
-            max_key = max(object, key=lambda k: object[k][1])
-            sorted_objects.append(max_key)
-            object.pop(max_key)
-    return sorted_objects
+def sort_objects(objects, choice):
+    if choice == "Weight":
+        return sorted(objects, key=lambda k: objects[k][0], reverse=True)
+    else:
+        return sorted(objects, key=lambda k: objects[k][1], reverse=True)
 
 def add_to_bag(sorted_objects, objects, max_weight=35):
     bag = []
@@ -44,45 +36,119 @@ def add_to_bag(sorted_objects, objects, max_weight=35):
     return bag, bag_value, current_weight
 
 def get_objects_not_in_bag(bag, objects):
-    not_in_bag = []
-    for object in objects:
-        if object in objects:
-            continue
-        else:
-            not_in_bag.append(object)
-    return not_in_bag
+    return [obj for obj in objects if obj not in bag]
 
 def get_solution_stats(solution, objects):
     weight = 0
     value = 0
-    for i in range(len(solution)):
-        current_object = solution[i]
-        current_weight, current_value = objects[current_object]
+    for obj in solution:
+        current_weight, current_value = objects[obj]
         weight += current_weight
         value += current_value
     return weight, value
 
 def random_optimize(solution, objects, max_weight=35, max_tries=100):
-    tries = 0
+    solution = solution.copy()
     leftover_bag = get_objects_not_in_bag(solution, objects)
-    while tries < max_tries:
-        choice = np.random.randint(0, 2) % 2
-        if choice == 0:
-            random_object_bag = random.choice(solution)
-            random_object_bag_idx = solution.index(random_object_bag)
-            random_object_leftover = random.choice(leftover_bag)
+
+    tries = 0
+
+    while tries < max_tries and solution:
+        choice = random.randint(0, 1)
+        if choice == 0 and leftover_bag:
+            rnd_in = random.choice(solution)
+            rnd_out = random.choice(leftover_bag)
+
             tmp_solution = solution.copy()
-            tmp_solution[random_object_bag_idx] = random_object_leftover
-            _, solution_value = get_solution_stats(solution, objects)
-            tmp_solution_weight, tmp_solution_value = get_solution_stats(tmp_solution, objects)
-            diff = tmp_solution_value - solution_value
-            if tmp_solution_weight > max_weight and diff > 0:
+            idx = tmp_solution.index(rnd_in)
+            tmp_solution[idx] = rnd_out
+            _, old_value = get_solution_stats(solution, objects)
+            new_weight, new_value = get_solution_stats(tmp_solution, objects)
+
+            diff = new_value - old_value
+
+            if new_weight <= max_weight and diff > 0:
                 solution = tmp_solution
+                leftover_bag.remove(rnd_out)
+                leftover_bag.append(rnd_in)
             else:
                 tries += 1
 
-        else: 
+        elif leftover_bag:
+            rnd_out = random.choice(leftover_bag)
+            w, _ = objects[rnd_out]
 
+            curr_weight, _ = get_solution_stats(solution, objects)
+
+            if curr_weight + w <= max_weight:
+                solution.append(rnd_out)
+                leftover_bag.remove(rnd_out)
+            else:
+                tries += 1
+        else:
+            break
+
+    return solution
+
+def greedy_optimize(solution, objects, max_weight=35, max_tries=100):
+    solution = solution.copy()
+    leftover_bag = get_objects_not_in_bag(solution, objects)
+
+    tries = 0
+
+    while tries < max_tries and solution:
+        choice = random.randint(0, 1)
+
+        if choice == 0 and leftover_bag:
+            rand_in = random.choice(solution)
+            old_weight, old_value = objects[rand_in]
+
+            best_swap = None
+            best_gain = 0
+
+            curr_weight, curr_value = get_solution_stats(solution, objects)
+
+            for obj in leftover_bag:
+                new_weight, new_value = objects[obj]
+
+                tmp_weight = curr_weight - old_weight + new_weight
+                gain = new_value - old_value
+
+                if tmp_weight <= max_weight and gain > best_gain:
+                    best_gain = gain
+                    best_swap = obj
+
+            if best_swap is not None:
+                idx = solution.index(rand_in)
+                solution[idx] = best_swap
+
+                leftover_bag.remove(best_swap)
+                leftover_bag.append(rand_in)
+            else:
+                tries += 1
+
+        elif leftover_bag:
+            curr_weight, _ = get_solution_stats(solution, objects)
+
+            best_item = None
+            best_value = 0
+
+            for obj in leftover_bag:
+                w, v = objects[obj]
+
+                if curr_weight + w <= max_weight and v > best_value:
+                    best_item = obj
+                    best_value = v
+
+            if best_item is not None:
+                solution.append(best_item)
+                leftover_bag.remove(best_item)
+            else:
+                tries += 1
+        else:
+            break
+
+    return solution
 
 
 if __name__ == "__main__":
@@ -90,18 +156,38 @@ if __name__ == "__main__":
     objects = create_objects(100)
     sorted_objects_weight = sort_objects(objects.copy(), "Weight")
     sorted_objects_value = sort_objects(objects.copy(), "Value")
-    print(f"All objects: {objects}")
-    print(f"Sorted after weight: {sorted_objects_weight}")
-    print(f"Sorted after value: {sorted_objects_value}")
     b1, b1_value, b1_weight = add_to_bag(sorted_objects_weight.copy(), objects.copy(), 35)
     b2, b2_value, b2_weight = add_to_bag(sorted_objects_value.copy(), objects.copy(), 35)
-    print(b1)
-    print(b2)
+    
+    print("Optimizing solution")
 
+    optimized_random_b1 = random_optimize(b1, objects)
+    optimized_random_b2 = random_optimize(b2, objects)
 
+    b1_weight,b1_value = get_solution_stats(b1, objects)
+    b2_weight,b2_value,  = get_solution_stats(b2, objects)
+    opt_random_b1_weight,opt_b1_value = get_solution_stats(optimized_random_b1, objects)
+    opt_random_b2_weight,opt_b2_value = get_solution_stats(optimized_random_b2, objects)
 
 
     
+    optimized_greedy_b1 = greedy_optimize(b1, objects)
+    optimized_greedy_b2 = greedy_optimize(b2, objects)
 
+    opt_greedy_b1_weight,opt_greedy_b1_value = get_solution_stats(optimized_greedy_b1, objects)
+    opt_greedy_b2_weight,opt_greedy_b2_value = get_solution_stats(optimized_greedy_b2, objects)
+
+    print("\nBEFORE OPTIMIZATION")
+    print(f"b1 value: {b1_value}, weight: {b1_weight}")
+    print(f"b2 value: {b2_value}, weight: {b2_weight}")
+
+    print("\nAFTER OPTIMIZATION")
+    print(f"optimized random b1 value: {opt_b1_value}, weight: {opt_random_b1_weight}")
+    print(f"optimized random b2 value: {opt_b2_value}, weight: {opt_random_b2_weight}")
+
+
+    print("\nAFTER GREEDY OPTIMIZATION")
+    print(f"optimized greedy b1 value: {opt_greedy_b1_value}, weight: {opt_greedy_b1_weight}")
+    print(f"optimized greedy b2 value: {opt_greedy_b2_value}, weight: {opt_greedy_b2_weight}")
 
 
--- knapsack/src/solutions.py
+++ knapsack/src/solutions.py
@@ -1,14 +1,31 @@
 class Solution:
     def __init__(self, objects, object_list):
-        self.objects = objects
-        self.object_list = object_list
-        self.value = 0
-        self.weight = 0
+        self.objects = objects # hold dict of all objects with their weight
+        self.object_list = object_list # list of objects, sorted by weight or value
+        self.total_value = 0
+        self.total_weight = 0
 
-    def update_value(self, new_value):
-        self.value = new_value
-        return self.value
+    def update_value(self, new_total_value):
+        self.total_value = new_total_value
+        return self.total_value
     
-    def update_weight(self, new_weight):
-        self.weight = new_weight
-        return self.weight
+    def update_weight(self, new_total_weight):
+        self.total_weight = new_total_weight
+        return self.total_weight
+    
+    def sort_weight(self):
+        sorted_objects = []
+        for i in range(len(object)):
+            max_key = max(self.objects, key=lambda k: self.objects[k][0])
+            print(f"MAXKEY: {max_key}")
+            sorted_objects.append(max_key)
+            object.pop(max_key)
+        return sorted_objects
+    
+    def sort_value(self):
+        sorted_objects = []
+        for i in range(len(object)):
+            max_key = max(self.objects, key=lambda k: self.objects[k][1])
+            sorted_objects.append(max_key)
+            object.pop(max_key)
+        return sorted_objects
